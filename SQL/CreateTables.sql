c71e8bffdd6db35ed2d6870a16f4b2774cee16ba
Neven <neven.kljajic@student.ipso.ch>
2023-03-02 17:37:31 +0100

    adjusted SQL file, but still need to add user-sql-command

--- SQL/CreateTables.sql
+++ SQL/CreateTables.sql
@@ -9,12 +9,24 @@ CREATE TABLE persons (
   email VARCHAR(255) NOT NULL
 );
 
+INSERT INTO persons (name, first_name, email)
+VALUES 
+('Potter', 'Harry', 'harry.potter@Zmail.com'),
+('Parker', 'Peter', 'spidey.parker@Smail.com'),
+('Baumeister', 'Bob', 'bob.B@Bmail.com');
+
 CREATE TABLE task_types (
   id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   color VARCHAR(255) NOT NULL,
   due_days VARCHAR(255) NOT NULL
 );
+
+INSERT INTO task_types (name, color, due_days)
+VALUES 
+('A', 'blue', '14'),
+('B', 'green', '7'),
+('C', 'red', '1');
 
 CREATE TABLE tasks (
   id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -27,3 +39,14 @@ CREATE TABLE tasks (
   FOREIGN KEY (person_id) REFERENCES persons(id),
   FOREIGN KEY (task_type_id) REFERENCES task_types(id)
 );
+
+INSERT INTO tasks (title, description, person_id, task_type_id, due_date, completed)
+VALUES
+('Clean', 'Clean the toilet', 3, 3, '2023-05-20', 0),
+('Shopping', 'Buy 1 KG Banana', 3, 2, '2023-05-02', 0),
+('Ollivanders', 'Choose a Wand', 1, 3, '2023-06-03', 0),
+('Work', 'Save NYC', 2, 3, '2023-08-11', 0),
+('Relationship', 'Go see Mary Jane', 2, 1, '2023-05-28', 0),
+('Relationship', 'Have fun with Ron and Hermione', 1, 1, '2023-05-06', 0),
+('School', 'Finish the Project', 2, 2, '2023-09-25', 0),
+('Shopping', 'Buy Bricks', 3, 2, '2023-10-10', 0);
